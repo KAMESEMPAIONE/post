324c9d6029c0a65536381298af77ebfb22e4ba63
KAMESEMPAIONE <volodyacy@gmail.com>
2023-03-06 19:19:36 +0200

    Added server url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import {logOut, setCredentials} from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://posts-api-qvua.onrender.com",
     credentials: "include",
     prepareHeaders: (headers, {getState}) => {
         const token = getState().auth.token
--- src/features/posts/EditPost/EditPost.jsx
+++ src/features/posts/EditPost/EditPost.jsx
@@ -2,13 +2,12 @@ import { useParams, Navigate } from "react-router-dom"
 import { PuffLoader } from "react-spinners"
 import { useGetAllPostsQuery } from "../postsApiSlice"
 import { useSelector } from "react-redux"
-import { selectCurrentId, selectCurrentRoles } from "../../auth/authSlice"
+import { selectCurrentId } from "../../auth/authSlice"
 import { EditPostForm } from "./EditPostForm"
 
 export const EditPost = () => {
     const { postId } = useParams()
     const userId = useSelector(selectCurrentId)
-    const userRoles = useSelector(selectCurrentRoles)
 
     const { post, ids } =  useGetAllPostsQuery("postsList", {
         selectFromResult: ({ data }) => ({
